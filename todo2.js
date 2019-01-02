8c26405f32a41d93d393b87df65000c1d6a3d44c
Jyoti Goyal <jyoti@Corp.Optym.net>
2019-01-02 20:53:55 +0530

    one function for changing tabs and displaying data respectively

--- todo2.js
+++ todo2.js
@@ -14,10 +14,11 @@ text1.addEventListener('keypress', function(event){
         //create li elements in ul
             let myvalue = limaker(text1.value);
 
-         //add item to local storage
+        //add item to local storage
             addItemToStorage(myvalue);
 
-    text1.value= ''; //clear input field
+        //clear input field
+            text1.value= ''; 
     }
     pendingToComplete();
 });
@@ -30,8 +31,8 @@ window.addEventListener("load", function(){
 });
 
 function getItemFromStorage(){
-        var a = localStorage.key(1);
-        var b = JSON.parse(localStorage.getItem(a));
+        let store = localStorage.key(1);
+        let b = JSON.parse(localStorage.getItem(store));
         for(i in b){
             limaker(b[i].data,b[i].state);
             addItemToStorage(b[i].data,b[i].state);
@@ -46,45 +47,75 @@ function getItemFromStorage(){
         let current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
+        console.log(this.id);
+        if(this.id=="completedtodo"){
+            console.log(this.innerText)
+            let li = document.getElementsByTagName("li");
+            for(let j = 0; j < li.length;j++){
+                if(li[j].className == "pending"){
+                    li[j].className += " hidden";
+                }
+                else{
+                    li[j].classList.remove("hidden");
+                }
+            }
+        }
+        else if(this.id=="pendingtodo"){
+            let li = document.getElementsByTagName("li");
+            for(let j = 0; j < li.length;j++){
+                if(li[j].className == "completed"){
+                    li[j].className += " hidden";
+                }
+                else{
+                    li[j].classList.remove("hidden");
+                }
+            } 
+        }
+        else{
+            let li = document.getElementsByTagName('li');
+            for(let j = 0; j < li.length;j++){
+                li[j].classList.remove("hidden");
+            } 
+        }
     });
   }
 
 //display all todos
-function displayall(){
-    document.getElementById("todotext").style.display="block";
-    for(i=0;i<completed.length;i++){
-        completed[i].style.display="block";
-    }
+// function displayall(){
+//     document.getElementById("todotext").style.display="block";
+//     for(i=0;i<completed.length;i++){
+//         completed[i].style.display="block";
+//     }
 
-    for(i=0;i<pending.length;i++){
-        pending[i].style.display="block";
-    }
-}
+//     for(i=0;i<pending.length;i++){
+//         pending[i].style.display="block";
+//     }
+// }
 
 //display pending todos
-function displaypending(){
-    document.getElementById("todotext").style.display="none";
-    for(i=0;i<completed.length;i++){
-        completed[i].style.display="none";
-    }
+// function displaypending(){
+//     document.getElementById("todotext").style.display="none";
+//     for(i=0;i<completed.length;i++){
+//         completed[i].style.display="none";
+//     }
 
-    for(i=0;i<pending.length;i++){
-        pending[i].style.display="block";
-    }
+//     for(i=0;i<pending.length;i++){
+//         pending[i].style.display="block";
+//     }
 
-}
+// }
 
 //display completed todos
-function displaycompleted(){
-    document.getElementById("todotext").style.display="none";
-    for(i=0;i<pending.length;i++){
-        pending[i].style.display="none";
-    }
+// function displaycompleted(){
+//     document.getElementById("todotext").style.display="none";
+//     for(i=0;i<pending.length;i++){
+//         pending[i].style.display="none";
+//     }
 
-    for(i=0;i<completed.length;i++){
-        completed[i].style.display="block";
-    }
-}
+//     for(i=0;i<completed.length;i++){
+//         completed[i].style.display="block";
+//     }
+// }
 
 //function to chnage pending task to completed
 function pendingToComplete(){
@@ -93,14 +124,14 @@ function pendingToComplete(){
             
             if(this.parentNode.className=="pending"){
                 this.parentNode.setAttribute("class", "completed");
-                let store = localStorage.key(0);
-                r = JSON.parse(localStorage.getItem(store));
+                let store = localStorage.key(1);
+                let r = JSON.parse(localStorage.getItem(store));
                 for(j in r){
-                    if(j==i-1){
-                        r[j] = "completed";
+                    if(j==i){
+                        r[j].state = "true";
                     }
                 }
-            localStorage.setItem('classes', JSON.stringify(r));
+                localStorage.setItem('items', JSON.stringify(r));
             }
         });
     } 
@@ -127,13 +158,11 @@ function limaker(itemvalue){
     let checkbox = document.createElement('input');
         checkbox.type = "checkbox";
         checkbox.setAttribute("class", "itemcheck");
-
-    
+        
     li.appendChild(checkbox);
-
-    //let label= document.createElement('label');
     li.appendChild(document.createTextNode(itemvalue));
     li.setAttribute("class", "pending");
+
     if(localStorage.getItem('classes') == null){
         classArray = [];
         classArray.push(li.className);
@@ -146,6 +175,5 @@ function limaker(itemvalue){
 
     //li.appendChild(label);
     ul.appendChild(li);
-    //console.log(li.innerText);
    return li.innerText;
 }
